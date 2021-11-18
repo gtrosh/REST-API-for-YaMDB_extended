1aa1bec2ee72c7c46328a5bc1e4ad08898bcb48f
Galina Troshkina <maystar2009@gmail.com>
2021-11-18 21:58:40 +0300

    corrected typo

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "default")
 
