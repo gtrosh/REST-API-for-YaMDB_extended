b6d767b43c4d05285f2d0ec99e49e74e38c79ef7
Galina Troshkina <maystar2009@gmail.com>
2021-11-23 23:52:17 +0300

    created redoc app

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django_filters",
     "users",
     "api",
+    "redoc",
 ]
 
 MIDDLEWARE = [
---
+++ redoc/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RedocConfig(AppConfig):
+    name = 'redoc'
